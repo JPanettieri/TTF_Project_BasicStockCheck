6b64461a1063b3978a5c28e2d7e74a895531bc61
JPanettieri <joshua@mach-tech.com.au>
2023-01-10 22:21:06 +1100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTTF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTTF")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89c670f4-131e-4c2e-a8b0-f4b675d36fa1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Binary_Price_Search_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var lines = File.ReadAllLines("TTFproducts.csv");
+            lines = lines.Skip(1).ToArray();
+            var list = new List<Product>();
+            foreach (var line in lines)
+            {
+                var values = line.Split(',');
+                if (values.Length == 3)
+                {
+                    var product = new Product()
+                    {
+                        Id = int.Parse(values[0]),
+                        ProductName = values[1],
+                        Price = double.Parse(values[2])
+                    };
+                    list.Add(product);
+                    list.Sort((x, y) => x.Price.CompareTo(y.Price));
+                }
+            }
+            //Find the centre of the list and start searching, depending on its value
+            var min = 0;
+            var max = list.Count - 1;
+            var mid = (max - min) / 2;
+            double searchPrice = double.Parse("5.49");
+            while (list[mid].Price < searchPrice)
+            {
+                if (mid < max)
+                {
+                    mid += 1;
+                }
+                else break;
+            }
+            while (list[mid].Price > searchPrice)
+            {
+                if (mid > min)
+                {
+                    mid -= 1;
+                }
+                else break;
+            }
+            if (list[mid].Price == searchPrice)
+            {
+                var priceSearched = new List<Product>() { list[mid] };
+                //show searched price
+                Console.WriteLine(priceSearched);
+            }
+            //If no products with the price searched is found, pop up a message for the user
+            else Console.WriteLine("No Products with this Price");
+        }
+    }
+}
